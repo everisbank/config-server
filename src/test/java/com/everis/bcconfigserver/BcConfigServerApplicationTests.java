8daf8c9f0066387f0c144e956f7137eda1d42f0b
DAlembert721 <dalembert.dmu@gmail.com>
2021-08-10 21:55:37 -0500

    feat: Add config server application

---
+++ src/test/java/com/everis/bcconfigserver/BcConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.everis.bcconfigserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BcConfigServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
